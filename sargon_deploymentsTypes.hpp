b64bfe93dc0023c6a0b683a073415be932574ad1
exoter <exoter@esa.int>
2017-03-14 13:59:33 +0100

    initial commit by rock-create-orogen

---
+++ sargon_deploymentsTypes.hpp
@@ -0,0 +1,15 @@
+#ifndef sargon_deployments_TYPES_HPP
+#define sargon_deployments_TYPES_HPP
+
+/* If you need to define types specific to your oroGen components, define them
+ * here. Required headers must be included explicitly
+ *
+ * However, it is common that you will only import types from your library, in
+ * which case you do not need this file
+ */
+
+namespace sargon_deployments {
+}
+
+#endif
+
